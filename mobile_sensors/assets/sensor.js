44b82eb6510bc1946ff56943327bddeaba4ffd4e
Silvia del Piano <silvia.delpiano27@gmail.com>
2020-05-19 11:52:02 +0200

    POST OK. Try to read better the data received

--- mobile_sensors/assets/sensor.js
+++ mobile_sensors/assets/sensor.js
@@ -1,6 +1,7 @@
 // This is the script that is executed by the browser and that will read the measurements made by
 // the accelerometer and that will send them to ThingsBoard
 
+// Wait until the browser is ready (everything loaded)
 $(document).ready(() => {
     try {
 
@@ -9,21 +10,28 @@ $(document).ready(() => {
         let y_coordinate = document.getElementById("y_coordinate");
         let z_coordinate = document.getElementById("z_coordinate");
 
+        // Check if the accelerometer is supported
         if ("Accelerometer" in window) {
 
+            // Create a new Accelerometer object
             let sensor = new Accelerometer({frequency: 1});
 
+            // Start the sensor reading
             sensor.start();
 
+            // Manage the reads (onreading event)
             sensor.onreading = () => {
                 x_coordinate.innerHTML = sensor.x;
                 y_coordinate.innerHTML = sensor.y;
                 z_coordinate.innerHTML = sensor.z;
 
+                // Logs
                 console.log("x coordinate: " + sensor.x + "\ny coordinate: " + sensor.y + "\nz coordinate:  " + sensor.z);
 
+                // Preapare data to be sent to the Node JS backend
                 let telemetry = { x: x_coordinate, y: y_coordinate, z: z_coordinate};
 
+                // POST data to the backend
                 fetch("/", {
                     method: "POST",
                     headers: {
@@ -34,7 +42,9 @@ $(document).ready(() => {
             }
 
         }
+        // The accelerometer is not supported
         else document.getElementById("error_message").innerHTML = "Accelerometer not supported";
+    // Manage errors
     } catch (error) {
         document.getElementById("error_message").innerHTML = error;
     }
