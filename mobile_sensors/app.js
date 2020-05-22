16df514999586faeaddcd7e39a55f5cfb6f1cf04
Silvia del Piano <silvia.delpiano27@gmail.com>
2020-05-22 16:40:15 +0200

    Try minor fixes

--- mobile_sensors/app.js
+++ mobile_sensors/app.js
@@ -16,9 +16,31 @@ app.use(express.static('./assets'));
 // Use JSON parser to handle POST data
 app.use(bodyParser.json());
 
+// Set up MQTT to send data to ThingsBoard
+var mqtt = require('mqtt');
+
+// ThingsBoard host
+const thingsboardHost = "demo.thingsboard.io";
+
+// Access token to connect to the Accelerometer device on ThingsBoard (stored in an config variable on Heroku)
+const accessToken = process.env.TS_ACCESS_TOKEN;
+
+// Log
+console.log("Connecting to: %s using accelerometer access token %s", thingsboardHost, accessToken);
+
+// Creating ThingsBoard client and connecting to the accelerometer
+var thingsboardClient = mqtt.connect('mqtt://'+thingsboardHost, {username: accessToken});
+
+// Log
+thingsboardClient.on("connect", function() {
+    console.log("Connection to ThingsBoard established");
+});
+
 //Start controller
 accelerometerController(app);
 
 //Listen to port
 app.listen(PORT);
 console.log(`You are listening to port ${PORT}`);
+
+//module.exports = thingsboardClient;
--- mobile_sensors/controllers/accelerometerController.js
+++ mobile_sensors/controllers/accelerometerController.js
@@ -1,25 +1,5 @@
 // This is the accelerometer controller. It also manages the HTTP requests.
 
-// Set up MQTT to send data to ThingsBoard
-var mqtt = require('mqtt');
-
-// ThingsBoard host
-const thingsboardHost = "demo.thingsboard.io";
-
-// Access token to connect to the Accelerometer device on ThingsBoard (stored in an config variable on Heroku)
-const accessToken = process.env.TS_ACCESS_TOKEN;
-
-// Log
-console.log("Connecting to: %s using accelerometer access token %s", thingsboardHost, accessToken);
-
-// Creating ThingsBoard client and connecting to the accelerometer
-var thingsboardClient = mqtt.connect('mqtt://'+thingsboardHost, {username: accessToken});
-
-// Log
-thingsboardClient.on("connect", function() {
-    console.log("Connection to ThingsBoard established");
-});
-
 // Manage HTTP request to the app
 module.exports = function(app) {
     
